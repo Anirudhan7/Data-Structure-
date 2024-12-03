747fb73dc9b6a4a024ff5dbb93d534a5b085eac6
Anirudhan7 <anirudhan2me@gmail.com>
2024-12-03 14:51:37 +0530

    first commit

---
+++ DSA/Singly/sample1.dart
@@ -0,0 +1,41 @@
+class Node {
+  int? data;
+  Node? next;
+  Node(this.data);
+}
+
+class SinglyLinkedList {
+  Node? head;
+  Node? tail;
+  void addNOde(int data) {
+    Node newNode = Node(data);
+    if (head == null) {
+      head = newNode;
+    } else {
+      tail?.next = newNode;
+    }
+    tail = newNode;
+  }
+
+  void display() {
+    if (head == null) {
+      print("EMPTY");
+    } else {
+      Node? temp = head;
+      while (temp != null) {
+        print(temp.data);
+        temp = temp.next;
+      }
+    }
+  }
+}
+
+void main() {
+  SinglyLinkedList list = SinglyLinkedList();
+  list.display();
+  list.addNOde(10);
+  list.addNOde(20);
+  list.addNOde(30);
+  list.addNOde(50);
+  list.display();
+}
